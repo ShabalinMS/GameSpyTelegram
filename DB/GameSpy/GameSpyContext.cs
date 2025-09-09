49d5f4a170e7349f4ce2d368a0b3ced0274a17f4
shabalinms63@gmail.com <shabalinms63@gmail.com>
2025-09-09 19:43:06 +0400

    ADD

---
+++ DB/GameSpy/GameSpyContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GameSpyTelegram.DB.GameSpy
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class GameSpyContext: DbContext
+    {
+        public DbSet<Player> Players { get; set; }
+
+        public GameSpyContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=GameSpyDev;Username=postgres;Password=postgres");
+        }
+
+    }
+}
